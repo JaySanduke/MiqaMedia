f8af21d9639d4002d571fc2924a2dc04135677cf
Satyam Sharma <satyamsharma1725@gmail.com>
2022-08-23 16:23:32 +0530

    add task path change

--- components/Cards/CardBoard.js
+++ components/Cards/CardBoard.js
@@ -1,7 +1,7 @@
 import { React, useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import Image from "next/dist/client/image";
-import AddTask from "./Modal/AddTask.";
+import AddTask from "components/Modal/AddTask.";
 import {
   ChevronDownIcon,
   PlusIcon,
