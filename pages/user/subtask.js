4ecf9ed327de3d891504ef2fb670ceeb405623b6
Jay Sanduke <Jaysanduke@gmail.com>
2022-08-29 08:48:21 +0530

    Update subtask.js

--- pages/user/subtask.js
+++ pages/user/subtask.js
@@ -99,7 +99,7 @@ export default function SubTask() {
           <CardSubTask wid={wpid} tid={tid} tabledata={tabledata} addSubtask={addSubtask} deleteSubtask={deleteSubtask}/>
         </div>
         <div className="w-full mb-12 px-4">
-          <CardSubTask wid={wpid} tid={tid} tabledata={tabledata} color="dark" deleteSubtask={deleteSubtask} />
+          <CardSubTask color="dark" wid={wpid} tid={tid} tabledata={tabledata} addSubtask={addSubtask} deleteSubtask={deleteSubtask} />
         </div>
       </div>
     </>
