7fae2ba5c3a4c4bb49641bfda97a2cc90555ab01
Satyam Sharma <satyamsharma1725@gmail.com>
2022-11-27 21:13:28 +0530

    save button

--- pages/user/editprofile.js
+++ pages/user/editprofile.js
@@ -101,14 +101,13 @@ export default function Profile() {
                     />
                   </div>
                   <div className="py-6 px-3 mt-32 sm:mt-0 text-center">
-                    <Link href="/user/workspace">
-                      <button
-                        className="bg-blueGray-700 active:bg-blueGray-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
-                        type="button"
-                      >
-                        Save Profile
-                      </button>
-                    </Link>
+                    <button
+                      onClick={{}}
+                      className="bg-blueGray-700 active:bg-blueGray-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
+                      type="button"
+                    >
+                      Save Profile
+                    </button>
                   </div>
                 </div>
 
